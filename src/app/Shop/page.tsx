4e60cc4b7e8b8004c61e92f36bcc51abfa9e1734
its-huzi-18 <157364561+its-huzi-18@users.noreply.github.com>
2025-01-19 21:47:35 +0500

    changes

--- src/app/Shop/page.tsx
+++ src/app/Shop/page.tsx
@@ -8,15 +8,17 @@ import { urlFor } from '@/sanity/lib/image';
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface ProductType{
-  _id:string,
-  name:string
-  image:any
-  price:number,
-  stockLevel:number
+interface ProductType {
+  _id: string;
+  name: string;
+  image: any;
+  price: number;
+  stockLevel: number;
 }
-const Shop = async() => {
-  const products:ProductType[] = await client.fetch(
+
+// Move data fetching to an async function
+async function getProducts(): Promise<ProductType[]> {
+  return await client.fetch(
     `*[_type == 'product']{
       _id,
       name,
@@ -25,39 +27,41 @@ const Shop = async() => {
       stockLevel
     }`
   );
+}
+
+const Shop = async () => {
+  const products = await getProducts(); // Fetch products
 
   return (
     <>
       <div>
         <MainImage tittle="Shop" />
-       <FilteredHead />
+        <FilteredHead />
 
         {/* Product Details */}
-        <div className='flex items-center justify-center gap-12 flex-wrap'>
-      {products.map((detail, i: number) => (
-        <div className='flex flex-col gap-3' key={detail._id}>
-          <Link href={`/DetailPage/${detail._id}`}>
-          <Image
-            src={urlFor(detail.image).url()}
-            width={300}
-            height={300}
-            alt={detail.name}
-            />
-            </Link>
-          <div className='w-[260px] gap-2 flex flex-col'>
-            <h3 className='font-medium text-[16px] w-[212px] '>
-              {detail.name}
-            </h3>
-            <div className='flex justify-between items-center'>
-            <h4 className=' font-medium'>
-              {detail.price}$
-            </h4>
-            <h4>Item:{detail.stockLevel}</h4>
+        <div className="flex items-center justify-center gap-12 flex-wrap">
+          {products.map((detail) => (
+            <div className="flex flex-col gap-3" key={detail._id}>
+              <Link href={`/DetailPage/${detail._id}`}>
+                <Image
+                  src={urlFor(detail.image).url()}
+                  width={300}
+                  height={300}
+                  alt={detail.name}
+                />
+              </Link>
+              <div className="w-[260px] gap-2 flex flex-col">
+                <h3 className="font-medium text-[16px] w-[212px]">
+                  {detail.name}
+                </h3>
+                <div className="flex justify-between items-center">
+                  <h4 className="font-medium">{detail.price}$</h4>
+                  <h4>Item: {detail.stockLevel}</h4>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
         {/* Pagination */}
         <div className="my-10">
           <Pagination />
